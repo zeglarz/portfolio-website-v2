b6f30ffc978fe8941fa3195338467195ae053fa2
Konrad Rudnicki <zeglarz@gmail.com>
2020-09-05 14:08:16 +0200

    skills constans

---
+++ src/views/About/constants.d.ts
@@ -0,0 +1,16 @@
+export enum LEVELS {
+    beginner = 'Beginner',
+    competent = 'Competent',
+    proficient = 'Proficient',
+    expert = 'Expert'
+};
+
+interface IStack {
+    name: string,
+    proficiency: LEVELS
+}
+
+export interface ITechs {
+    title: string,
+    stack: IStack[]
+}
---
+++ src/views/About/constants.ts
@@ -0,0 +1,128 @@
+import {ITechs, LEVELS} from './constants.d'
+
+const techs: ITechs[] = [
+    {
+        title: 'Front-end technologies',
+        stack: [
+            {
+                name: 'HTML',
+                proficiency: LEVELS.proficient
+            },
+            {
+                name: 'CSS',
+                proficiency: LEVELS.proficient
+            },
+            {
+                name: 'JavaScript',
+                proficiency: LEVELS.proficient
+            },
+            {
+                name: 'React JS',
+                proficiency: LEVELS.proficient
+            },
+            {
+                name: 'Sass',
+                proficiency: LEVELS.competent
+            },
+            {
+                name: 'Gatsby',
+                proficiency: LEVELS.beginner
+            },
+            {
+                name: 'Styled Components',
+                proficiency: LEVELS.proficient
+            }
+        ]
+    },
+    {
+        title: 'Back-end technologies',
+        stack: [
+            {
+                name: 'NodeJS',
+                proficiency: LEVELS.competent
+            },
+            {
+                name: 'PostgreSQL',
+                proficiency: LEVELS.beginner
+            },
+            {
+                name: 'MongoDB',
+                proficiency: LEVELS.competent
+            },
+            {
+                name: 'Express',
+                proficiency: LEVELS.competent
+            }
+        ]
+    },
+    {
+        title: 'Software & tools',
+        stack: [
+            {
+                name: 'VSCode',
+                proficiency: LEVELS.competent
+            },
+            {
+                name: 'Postman',
+                proficiency: LEVELS.competent
+            },
+            {
+                name: 'Adobe Xd',
+                proficiency: LEVELS.beginner
+            },
+            {
+                name: 'Adobe Illustrator',
+                proficiency: LEVELS.beginner
+            },
+            {
+                name: 'GitHub',
+                proficiency: LEVELS.competent
+            },
+            {
+                name: 'Sketch',
+                proficiency: LEVELS.competent
+            }
+        ]
+    },
+    {
+        title: 'I use these as well',
+        stack: [
+            {
+                name: 'Git',
+                proficiency: LEVELS.competent
+            },
+            {
+                name: 'Firebase',
+                proficiency: LEVELS.beginner
+            },
+            {
+                name: 'GraphQL',
+                proficiency: LEVELS.beginner
+            },
+            {
+                name: 'Redux',
+                proficiency: LEVELS.competent
+            },
+            {
+                name: 'Jest',
+                proficiency: LEVELS.beginner
+            },
+            {
+                name: 'Socket IO',
+                proficiency: LEVELS.beginner
+            },
+            {
+                name: 'Apollo',
+                proficiency: LEVELS.beginner
+            },
+            {
+                name: 'NPM',
+                proficiency: LEVELS.proficient
+            },
+            {
+                name: 'Webpack',
+                proficiency: LEVELS.competent
+            }
+        ]
+    }
+];
