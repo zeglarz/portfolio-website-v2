b150b36255cef122c7d5f9b2b960d19075582738
Konrad Rudnicki <zeglarz@gmail.com>
2020-09-29 13:14:37 +0200

    styling the contact form

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from 'react';
 import { Formik } from 'formik';
 import { Button, TextField } from '@material-ui/core';
 import email from 'emailjs-com';
+import styled from 'styled-components';
 
 
 interface ContactFormProps {
@@ -15,40 +16,70 @@ const initialValues = {
     message: '',
 };
 
+const StyledForm = styled.div`
+width: 100%;
+  margin: 0 auto;
+  max-width: 400px;
+  margin-inline-end: 2rem;
+  form {
+    display: grid;
+    grid-gap: 1rem;
+    button {
+      justify-content: center;
+      height: 45px;
+    }
+  }
+form div {
+width: 100%;
+display: block;
+}
+`;
+
 const ContactForm: FunctionComponent<ContactFormProps> = () => {
     return (
-        <Formik
-            initialValues={initialValues}
-            onSubmit={async (values, { setErrors }) => {
-            }}
-        >
-            {({ isSubmitting }) =>
-                (
-                    <form>
-                        <TextField
-                            name={'name'}
-                            label={'Name'}
-                            type={'text'}
-                        />
-                        <TextField
-                            name={'email'}
-                            label={'Email'}
-                            type={'email'}
-                        />
-                        <TextField
-                            name={'title'}
-                            label={'Title'}
-                            type={'text'}
-                        />
-                        <TextField
-                            name={'message'}
-                            label={'Your Message'}
-                            type={'text'}
-                        />
-                        <Button>Send</Button>
-                    </form>
-                )}
-        </Formik>);
+        <StyledForm>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={async (values, { setErrors }) => {
+                }}
+            >
+                {({ isSubmitting }) =>
+                    (
+                        <form>
+                            <TextField
+                                variant='filled'
+                                name={'name'}
+                                label={'Name'}
+                                type={'text'}
+                            />
+                            <TextField
+                                variant='filled'
+                                name={'email'}
+                                label={'Email'}
+                                type={'email'}
+                            />
+                            <TextField
+                                variant='filled'
+                                name={'title'}
+                                label={'Title'}
+                                type={'text'}
+                            />
+                            <TextField
+                                variant='filled'
+                                name={'message'}
+                                label={'Your Message'}
+                                type={'text'}
+                            />
+                            <Button
+                                variant='outlined'
+                            >
+                                Send
+                            </Button>
+                        </form>
+                    )}
+            </Formik>
+        </StyledForm>
+    );
 };
 
 export default ContactForm;
